a27e27ab02792f81f914d349ede655c3e03a31c7
rahul <rahul.pandey@thrymr.net>
2022-02-05 16:07:49 +0530

    emplimentation of the stete mutation in Class based Components

---
+++ src/Student1.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+export default class Student1 extends React.Component {
+  
+    render() {
+    //must in rcc
+    console.log(this.props);
+    return (
+      <div
+        style={{
+          backgroundColor: "coral",
+          margin: 20,
+          padding: 10,
+          color: "darkred",
+        }}
+      >
+        <h1>Student1 Component</h1>
+        <p>a class based component</p>
+        <h2>Student:{this.props.name}</h2>
+        <h2>Email:{this.props.email}</h2>
+      </div>
+    );
+  }
+}
+// props can be changed only in the parent component 
+// cannot change in here 
