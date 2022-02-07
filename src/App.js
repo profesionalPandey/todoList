631b554c945d0b5cbc6a2fe7605dac481e9d6684
rahul <rahul.pandey@thrymr.net>
2022-02-07 10:58:48 +0530

    toggle if/else in fn() based Components

--- src/App.js
+++ src/App.js
@@ -7,6 +7,10 @@ import User from "./User";
 import Student from "./Student";
 import { useState } from "react"; //stateHook
 import Student1 from "./Student1";
+import React from "react";
+import Profile from "./Profile";
+import User2 from "./User2";
+import Members from "./Members";
 
 // all about the components rfc rcc
 function App() {
@@ -17,8 +21,32 @@ function App() {
   //   alert(data);
   // }
 
+  // fn()for onChange event
+  const [data, setData] = useState(null);
+  const [print, setPrint] = useState(false);
+  function getData(val) {
+    console.log(val.target.value);
+    setData(val.target.value);
+    setPrint(false);
+  }
+  // submit event
+  const [name, setName] = useState("");
+  const [tnc, setTnc] = useState(false);
+  const [intrust, setIntrust] = useState("");
+
+  function getFormData(e) {
+    console.log(name, tnc, intrust);
+    // stops lode
+    e.preventDefault();
+  }
+  // onclick event
+  const [status, setStatus] = React.useState(true);
   // using stateHooks
   const [n, setN] = useState("Rahul");
+  // user2 
+  function getData(){
+    alert("from app");
+  }
   console.warn("___________check for rerendering__________");
   return (
     <div className="App">
@@ -46,6 +74,7 @@ function App() {
       <User />
       {/* rcc */}
       <User1 />
+      
       <Student name={n} />
       <button
         onClick={() => {
@@ -65,6 +94,76 @@ function App() {
       {/* <Apple/> */}
       {/* {Apple()} */}
 
+      {/* native components */}
+      <div
+        style={{
+          backgroundColor: "MistyRose",
+          margin: 20,
+          padding: 10,
+          color: "brown",
+        }}
+      >
+        {print ? <h1>{data}</h1> : null}
+        <p>Get INput Box Value</p>
+        <input type="text" onChange={getData} />
+        <button onClick={() => setPrint(true)}>Print</button>
+      </div>
+
+      <div
+        style={{
+          backgroundColor: "MistyRose",
+          margin: 20,
+          padding: 10,
+          color: "brown",
+        }}
+      >
+        {status ? <h1>magic!!</h1> : null}
+        <p>Show/Hide & Toggle</p>
+        <button onClick={() => setStatus(true)}>Show</button>
+        <button onClick={() => setStatus(false)}>Hide</button>
+        <button onClick={() => setStatus(!status)}>Toggle</button>
+      </div>
+
+      <div
+        style={{
+          backgroundColor: "MistyRose",
+          margin: 20,
+          padding: 10,
+          color: "brown",
+        }}
+      >
+        {/* forms and there property */}
+        <h1>Forms!!</h1>
+        <form onSubmit={getFormData}>
+          <input
+            type="text"
+            placeholder="enter name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <br />
+          <br />
+          <select onChange={(e) => setIntrust(e.target.value)}>
+            <option>Select Option</option>
+            <option>Dark</option>
+            <option>Light</option>
+          </select>
+          <br />
+          <br />
+          <input type="checkbox" onChange={(e) => setTnc(e.target.checked)} />
+          <span>Accept Terms & conditions</span>
+          <br />
+          <br />
+          <button type="submit">Submit</button>
+          <button>Clear</button>
+        </form>
+      </div>
+
+        <Profile/>
+        <User2 data={getData} />
+        <div style={{float:"right"}}>
+        <Members/>
+        </div>
       <h5>App.js</h5>
     </div>
   );
